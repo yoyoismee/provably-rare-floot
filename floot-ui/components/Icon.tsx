53d9e3cf8998713352199296a1754ce85a8c5916
Chinnatip <cupuri.on234@gmail.com>
2021-09-18 00:36:12 +0700

    icon#2

--- floot-ui/components/Icon.tsx
+++ floot-ui/components/Icon.tsx
@@ -12,7 +12,8 @@ const Icon = ({ fill, noMargin = false }: Props) => {
   return (
     <FontAwesomeIcon
       icon={fill}
-      className={`${noMargin ? '' : 'mr-3'} fa-show`}
+      className={`${noMargin ? '' : 'mr-3'} fa-show inline`}
+      style={{ width: '28px' }}
     />
   )
 }
