775348211bf8ca91a214707f070a1f9fc5ea6f35
Chinnatip <cupuri.on234@gmail.com>
2021-09-18 01:48:03 +0700

    floodcard added

---
+++ floot-ui/static/flootLists.tsx
@@ -0,0 +1,48 @@
+export const lists = [
+    {
+        price: 4,
+        howTo:"*[วิธี] [carb] with *[opt ที่มา 2][veg, protein] | and [veg, protein]|served with [drink]",
+        ingredient: [ 'int1','int2','int3','int4','bonus' ],
+        flootId: "floot.center" 
+    },
+    {
+        price: 4,
+        howTo:"*[วิธี] [carb] with *[opt ที่มา 2][veg, protein] | and [veg, protein]|served with [drink]",
+        ingredient: [ 'int1','int2','int3','int4','bonus' ],
+        flootId: "floot.center" 
+    },
+    {
+        price: 4,
+        howTo:"*[วิธี] [carb] with *[opt ที่มา 2][veg, protein] | and [veg, protein]|served with [drink]",
+        ingredient: [ 'int1','int2','int3','int4','bonus' ],
+        flootId: "floot.center" 
+    },
+    {
+        price: 4,
+        howTo:"*[วิธี] [carb] with *[opt ที่มา 2][veg, protein] | and [veg, protein]|served with [drink]",
+        ingredient: [ 'int1','int2','int3','int4','bonus' ],
+        flootId: "floot.center" 
+    },
+    {
+        price: 4,
+        howTo:"*[วิธี] [carb] with *[opt ที่มา 2][veg, protein] | and [veg, protein]|served with [drink]",
+        ingredient: [ 'int1','int2','int3','int4','bonus' ],
+        flootId: "floot.center" 
+    },
+    {
+        price: 4,
+        howTo:"*[วิธี] [carb] with *[opt ที่มา 2][veg, protein] | and [veg, protein]|served with [drink]",
+        ingredient: [ 'int1','int2','int3','int4','bonus' ],
+        flootId: "floot.center" 
+    },
+    {
+        price: 4,
+        howTo:"*[วิธี] [carb] with *[opt ที่มา 2][veg, protein] | and [veg, protein]|served with [drink]",
+        ingredient: [ 'int1','int2','int3','int4','bonus' ],
+        flootId: "floot.center" 
+    },
+]
+
+
+
+ 
